cdf7fa2e181cd179fcfa4ecb4f6a60accdf741f6
unknown <michaelbt2@gmail.com>
2026-05-24 19:58:28 -0500

    Add Vercel Analytics and Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import './globals.css'
 import AmplitudeProvider from './components/AmplitudeProvider'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export const metadata: Metadata = {
   title: {
@@ -50,6 +52,8 @@ export default function RootLayout({
         </head>
         <body>
           <AmplitudeProvider/>
+          <Analytics />
+          <SpeedInsights />
           {children}
         </body>
       </html>
